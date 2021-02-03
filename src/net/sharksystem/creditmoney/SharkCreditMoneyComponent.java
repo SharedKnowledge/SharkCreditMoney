92c8611512aa697bfa924597ed236be88e928a3c
thsc42 <thomas.schwotzer@htw-berlin.de>
2021-02-03 09:14:10 +0100

    added shark component interface

---
+++ src/net/sharksystem/creditmoney/SharkCreditMoneyComponent.java
@@ -0,0 +1,8 @@
+package net.sharksystem.creditmoney;
+
+import net.sharksystem.ASAPFormats;
+
+@ASAPFormats(formats = {SharkCreditMoneyComponent.SHARK_CREDIT_MONEY_FORMAT})
+public interface SharkCreditMoneyComponent {
+    public static final String SHARK_CREDIT_MONEY_FORMAT = "shark/money";
+}
